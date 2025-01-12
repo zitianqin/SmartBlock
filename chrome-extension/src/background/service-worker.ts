88e7b63766b315197d974521d9457e140f172ac0
Zitian Qin <zitianqin2015@gmail.com>
2025-01-12 20:12:01 +1100

    Add redirect (to invalid page) and refactor existing pages

---
+++ chrome-extension/src/background/service-worker.ts
@@ -0,0 +1,27 @@
+export const redirectExtensionPath = "/#blocked";
+
+// Rule to block Google
+const BLOCK_RULE = {
+  id: 1,
+  action: {
+    type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+    redirect: {
+      extensionPath: redirectExtensionPath,
+    },
+  },
+  condition: {
+    urlFilter: "*://*.google.com*",
+    resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME],
+  },
+};
+
+// Add blocking rule
+chrome.declarativeNetRequest.updateDynamicRules({
+  removeRuleIds: [BLOCK_RULE.id],
+  addRules: [BLOCK_RULE],
+});
+
+// Store rule in local storage
+chrome.storage.local.set({
+  blocked_sites: ["*://*.google.com*"],
+});
---
+++ chrome-extension/src/content/main.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+
+// (async () => {
+//   const contentRoot = document.createElement("div");
+//   contentRoot.id = "blocked-page-content-root";
+//   document.body.append(contentRoot);
+//   const root = createRoot(contentRoot, {
+//     identifierPrefix: "blocked-page",
+//   });
+
+//   root.render(
+//     <React.StrictMode>
+//       <h1>You have been blocked!</h1>
+//     </React.StrictMode>
+//   );
+// })();
